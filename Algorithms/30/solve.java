9380d8d97e8b224327ca3c42fbbf70c592a3eee5
xiaok0707 <1152875082@qq.com>
2020-03-25 17:11:39 +0800

    udpate 30th problem

--- Algorithms/30/solve.java
+++ Algorithms/30/solve.java
@@ -1,15 +1,29 @@
-import java.math.*;
-
 class Solution {
-    public int divide(int dividend, int divisor) {
-        BigInteger a=new BigInteger(String.valueOf(dividend));
-        BigInteger b=new BigInteger(String.valueOf(divisor));
-        a=a.divide(b);
-        BigInteger INT_MAX=new BigInteger(String.valueOf(Integer.MAX_VALUE));
-        BigInteger INT_MIN=new BigInteger(String.valueOf(Integer.MIN_VALUE));
-        if(a.compareTo(INT_MAX)<=0 && a.compareTo(INT_MIN)>=0){
-            return a.intValue();
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> ans=new ArrayList<>();
+        if(s.isEmpty() || 0==words.length) return ans;
+        int one_len=words[0].length();
+        int all_num=words.length;
+        HashMap<String,Integer> words_map=new HashMap<>();
+        for(int i=0;i<all_num;++i)
+            words_map.put(words[i],1+words_map.getOrDefault(words[i],0));
+        for(int i=0;i<one_len;++i){
+            int cnt=0,le=i,ri=i;
+            HashMap<String,Integer> cur_map=new HashMap<>();
+            while(ri+one_len<=s.length()){
+                String r_word=s.substring(ri,ri+one_len);
+                cur_map.put(r_word,1+cur_map.getOrDefault(r_word,0));
+                cnt+=1;
+                ri+=one_len;
+                while(cur_map.getOrDefault(r_word,0)>words_map.getOrDefault(r_word,0)){
+                    String l_word=s.substring(le,le+one_len);
+                    cur_map.put(l_word,cur_map.get(l_word)-1);
+                    cnt-=1;
+                    le+=one_len;
+                }
+                if(cnt==all_num) ans.add(le);
+            }
         }
-        else return Integer.MAX_VALUE;
+        return ans;
     }
 }
