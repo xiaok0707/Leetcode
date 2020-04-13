2ff091872c05658cbfc3fcfcc9b90fb8a4d2d487
xiaok0707 <1152875082@qq.com>
2020-04-13 12:57:52 +0800

    add 69th problem

---
+++ Algorithms/69/solve.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int le=0,ri=50000;
+        while(le+1<ri){
+            int mid=(le+ri)>>1;
+            if(1LL*mid*mid<=x) le=mid;
+            else ri=mid;
+        }
+        return le;
+    }
+};
