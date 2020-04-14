81afbdbf0a33bb0a588c912cd84273d1447245f2
xiaok0707 <1152875082@qq.com>
2020-04-14 10:08:00 +0800

    add 75th problem

---
+++ Algorithms/75/solve.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        cnt=[0,0,0]
+        for e in nums: cnt[e]+=1
+        pos=0
+        for val in range(3):
+            for num in range(cnt[val]):
+                nums[pos]=val
+                pos+=1
