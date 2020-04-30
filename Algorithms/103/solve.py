ef7942878b94d0de705a9c4674fb962a814114b0
xiaok0707 <1152875082@qq.com>
2020-04-30 09:52:03 +0800

    add 102nd problem

---
+++ Algorithms/103/solve.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None: return []
+        que,ans,floor=[root],[],0
+        while len(que)>0:
+            sz,res=len(que),[]
+            for i in range(sz):
+                tp=que.pop(0)
+                res.append(tp.val)
+                if tp.left: que.append(tp.left)
+                if tp.right: que.append(tp.right)
+            ans.append(res)
+            floor+=1
+        return ans
