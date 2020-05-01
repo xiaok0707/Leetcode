e1d74ccd219fd7482c2aefa3505e37273a56042f
xiaok0707 <1152875082@qq.com>
2020-05-01 09:01:48 +0800

    add 104th problem

---
+++ Algorithms/104/solve.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        queue<TreeNode*> que;
+        int floor=0;
+        que.push(root);
+        while(!que.empty()){
+            int len=que.size();
+            for(int i=0;i<len;++i){
+                TreeNode* x=que.front();que.pop();
+                if(x->left) que.push(x->left);
+                if(x->right) que.push(x->right);
+            }
+            floor+=1;
+        }
+        return floor;
+    }
+};
